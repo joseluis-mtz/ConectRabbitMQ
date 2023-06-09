6f2639366b95daeb3f88e61b0fa2b97c09c4b7d0
Jose Luis Martinez <joseluismartinez1024@gmail.com>
2023-06-08 20:06:24 -0600

    Iniciar conexion enviar mensaje
    Cuerpo del metodo

--- SCRabbit/Producer/Program.cs
+++ SCRabbit/Producer/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using RabbitMQ.Client;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,6 +13,24 @@ namespace Producer
         {
             // Este es el SENDER-MENSAJERO
 
+            // Crear Conexion
+            var Fabrica = new ConnectionFactory
+            {
+                HostName = "localhost",
+            };
+
+            // Usar la conexion
+            using (var objConexion = Fabrica.CreateConnection())
+            using (var objCanal = objConexion.CreateModel())
+            {
+                objCanal.QueueDeclare("ColaUno", false, false, false, null);
+                var Mensaje = "Mensaje de bienvenida";
+                // Convertir mensaje a una coleccion de bytes
+                var Cuerpo = Encoding.UTF8.GetBytes(Mensaje);
+                objCanal.BasicPublish("", "ColaUno", null, Cuerpo);
+                Console.WriteLine("Mensaje enviado");
+            }
+            Console.ReadKey();
         }
     }
 }
